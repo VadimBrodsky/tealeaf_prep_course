c84cacff8396488707e45d4901404655b05086e7
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-04-28 22:25:56 -0400

    Finish Loops chapter

---
+++ intro_ruby/06_loops/02_while_stop.rb
@@ -0,0 +1,7 @@
+action = ''
+
+while action != 'STOP'
+  print "Do something! "
+  action = gets.chomp
+  puts "I just did #{action}"
+end
---
+++ intro_ruby/06_loops/03_array_with_index.rb
@@ -0,0 +1,4 @@
+dates = [1999, 2010, 2014, 1979, 2009]
+
+dates.each_with_index do |item, index| puts "#{index + 1}. #{item}"
+end
