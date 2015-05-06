cec4d884ca68938b2cf3a57200464236581edbf2
Vadim Brodsky <vadimb@crankworks.ca>
2015-05-06 08:36:34 -0400

    Start Question

---
+++ workbook/intermediate/quiz_2/exercise_02.rb
@@ -0,0 +1,13 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" }
+}
+
+# p munsters.sort_by do |munster|
+#   munster.key.lenght
+# end
+
+p munsters.keys
+p munsters["Herman"]
