d77428b6c64ccdaee1bdbd3b52ab64480263661f
Vadim Brodsky <vadimb@crankworks.ca>
2015-05-07 07:59:54 -0400

    Add tealeaf solution

--- workbook/intermediate/quiz_2/exercise_05.rb
+++ workbook/intermediate/quiz_2/exercise_05.rb
@@ -1,3 +1,10 @@
 sentence = "Humpty Dumpty sat on a wall."
 
+# My solution
 p sentence[0, sentence.length - 1].split.reverse.join(" ") + "."
+
+
+# Tealeaf solution
+# words = sentence.split(/\W/)
+# words.reverse!
+# backwards_sentence = words.join(' ') + '.'
