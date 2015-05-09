e6e65c16d7735a373c15d9b56d593ce3737c4177
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-05-09 16:11:15 -0400

    Finish E2

--- workbook/advanced/exercise_02.rb
+++ workbook/advanced/exercise_02.rb
@@ -1,6 +1,15 @@
 greetings = { a: 'hi' }
+puts "greetings: #{greetings.object_id}"
+puts "greetings a: object: #{greetings[:a].object_id}"
+
 informal_greeting = greetings[:a]
+puts "informal_greeting: #{informal_greeting.object_id}"
 informal_greeting << ' there'
+p informal_greeting
 
 puts informal_greeting  #  => "hi there"
-puts greetings
+puts greetings  				#  => {:a => "hi there"}
+
+# informal_greeting is referencing the string object inside of the greetings
+# hash, therefore when it's modified with the << operator the original
+# greetings a: string is modified
