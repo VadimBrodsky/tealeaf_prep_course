4c29850a55e3113c431c84800a3c9ef722d537aa
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-05-09 15:34:25 -0400

    Finished E5

--- workbook/intermediate/quiz_3/exercise_05.rb
+++ workbook/intermediate/quiz_3/exercise_05.rb
@@ -1,7 +1,25 @@
-def color_valid
+# Original method
+def color_valid_a
+	color = "red"
   if color == "blue" || color == "green"
     true
   else
     false
   end
 end
+
+# Using a tertiary operator
+def color_valid_b
+	color = "red"
+  color == "blue" || color == "green" ? true : false
+end
+
+# Using the implied return of the logical evaluation
+def color_valid_c
+	color = "red"
+	color == "blue" || color == "green"
+end
+
+puts color_valid_a
+puts color_valid_b
+puts color_valid_c
