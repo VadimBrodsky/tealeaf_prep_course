586cc0b372f1a3041733fbc37d7d7dfec63749e9
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-05-09 17:49:53 -0400

    Add another implementation without the secure library

--- workbook/advanced/exercise_04.rb
+++ workbook/advanced/exercise_04.rb
@@ -1,6 +1,6 @@
 require 'securerandom'
 
-def uuid
+def uuid_secure
 	format_sections = [8, 4, 4, 4, 12]
 	full_uuid = []
 
@@ -11,4 +11,18 @@ def uuid
 	full_uuid.join('-')
 end
 
-puts uuid
+
+def uuid_manual
+	hex_characters = (0..9).to_a + ('a'..'f').to_a
+	uuid_format = [8, 4, 4, 4, 12]
+	full_uuid = []
+
+	uuid_format.each do |digits|
+		full_uuid << hex_characters.sample(digits).join
+	end
+	full_uuid.join('-')
+end
+
+
+puts uuid_secure
+puts uuid_manual
