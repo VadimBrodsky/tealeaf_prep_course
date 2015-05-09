6acec978da76dcacc3d1fdd266bf98ff5292396f
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-05-09 15:41:32 -0400

    Finish E1

--- workbook/advanced/exercise_01.rb
+++ workbook/advanced/exercise_01.rb
@@ -3,3 +3,15 @@ if false
 end
 
 greeting
+puts greeting
+p greeting.class
+p greeting.nil?
+
+# The conditional is never run because it's false
+# The greeting variable is defined but is not initialized with any value
+# therefore it is set to nil
+
+# Typically, when you reference an uninitialized variable, Ruby will raise an
+# exception, stating that it's undefined. However, when you initialize a local
+# variable within an if block, even if that if block doesn't get executed, the
+# local variable is initialized to nil.
