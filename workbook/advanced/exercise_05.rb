bdba1fa3234241069d846325cdb5a8925520d5f5
Vadim Brodsky <vadimb@crankworks.ca>
2015-05-11 08:39:47 -0400

    Improved answer based on tealeaf answer

--- workbook/advanced/exercise_05.rb
+++ workbook/advanced/exercise_05.rb
@@ -1,28 +1,28 @@
-statement = "Herman Munster is a BIG BIG man."
+statement = "Herman Munster is a BIG BIG man!"
 
 class String
 	def map_words!
 		words = self
-		if words[words.length - 1] =~ /[.?!]/
+
+		# check the last character for punctuation
+		if words[words.length - 1] =~ /\p{Punct}$/
 			punctuation = words[words.length - 1]
-			words.chop!
-		end
-		words = words.split
-
-		words.each do |word|
-			yield word
 		end
 
+		# split the string based on word regex
+		words = words.split(/\W/)
+
+		# yield the map iterator
+		words.map!{ |word| yield word}
+
+		# combine and add punctuation if needed
 		words = words.join(' ')
+		words += punctuation if punctuation
 
-		if punctuation
-			words += punctuation
-		end
-
-		words
-
+		# return the self object
+		self.replace(words)
 	end
 end
 
 # statement.map_words! { |word| puts word }
-puts statement.map_words! {|word| word.reverse! }
+puts statement.map_words! {|word| word.reverse }
